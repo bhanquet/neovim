84624b3b4396bca94aca2a120a6ba1af8f56feb1
Brian Hanquet <apps.brinat@pm.me>
2024-08-05 12:55:28 +0200

    Edit tree to sync cwd

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -24,6 +24,11 @@ return {
     {
         "nvim-tree/nvim-tree.lua",
         opts = {
+            sync_root_with_cwd = true,
+            respect_buf_cwd = true,
+            update_focused_file = {
+                enable = true,
+            },
             actions = {
                 open_file = {
                     quit_on_open = true
