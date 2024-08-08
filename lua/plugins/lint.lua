f471dc5a5adf22a9c622311c847bb9bc4504b706
Brian Hanquet <apps.brinat@pm.me>
2024-08-08 22:06:03 +0200

    And lint

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			require("lint").linters_by_ft = {}
+
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
+	{
+		"rshkarin/mason-nvim-lint",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-lint",
+		},
+		opts = {
+			ensure_installed = {},
+			automatic_installation = true,
+		},
+	},
+}
