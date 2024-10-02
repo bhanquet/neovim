c550034a772f6d13b92812770d9bc047581bf330
Brian Hanquet <apps.brinat@pm.me>
2024-10-02 20:56:27 +0200

    Disable lsp automatic install

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,12 +20,12 @@ return {
 			},
 		},
 	},
-	{
-		"zapling/mason-conform.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"stevearc/conform.nvim",
-		},
-		opts = {},
-	},
+	--	{
+	--		"zapling/mason-conform.nvim",
+	--		dependencies = {
+	--			"williamboman/mason.nvim",
+	--			"stevearc/conform.nvim",
+	--		},
+	--		opts = {},
+	--	},
 }
