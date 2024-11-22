d44468408c09ca43d8991082e767960b62a7dd72
Brian Hanquet <apps.brinat@pm.me>
2024-11-22 21:49:18 +0100

    Refactoring + Lazy update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = function ()
-        local builtin = require('telescope.builtin')
-        return {
-            {'<leader>ff', builtin.find_files, desc = "Find file"},
-            {'<leader>fg', builtin.live_grep, desc = "Find grep"},
-            {'<leader>fe', builtin.git_files, desc = "Find git"},
-            {'<leader>fb', builtin.buffers, desc = "Find buffer"},
-            {'<leader>fh', builtin.help_tags, desc = "Find help"},
-        }
-    end,
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = function()
+		local builtin = require("telescope.builtin")
+		return {
+			{ "<leader>ff", builtin.find_files, desc = "Find file" },
+			{ "<leader>fg", builtin.live_grep, desc = "Find grep" },
+			{ "<leader>fe", builtin.git_files, desc = "Find git" },
+			{ "<leader>fb", builtin.buffers, desc = "Find buffer" },
+			{ "<leader>fh", builtin.help_tags, desc = "Find help" },
+		}
+	end,
 }
